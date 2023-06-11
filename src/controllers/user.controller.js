99158ccf22c390658e306fc7bd10de77b52f1006
MathePLay <Matheus.doer@gmail.com>
2023-06-11 10:02:29 -0300

    End of Class #09

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,23 @@
-const create = (req, res) => {
+const userService = require("../services/user.service")
+const mongoose = require("mongoose")
+
+const create = async (req, res) => {
     const {name, userName, email, password, avatar, background} = req.body
 
     if (!name || !userName || !email || !password || !avatar || !background){
         res.status(400).send({message: "submit all fields for registration"})
     }
+
+    const user = await userService.createService(req.body)
+
+    if (!user){
+        return res.status(400).send({message: "Error creating User"})
+    }
     
     res.status(201).send({
         message: "User created sucessfully",
         user: {
+            id: user._id,
             name, 
             userName, 
             email, 
@@ -17,4 +27,31 @@ const create = (req, res) => {
     })
 }
 
-module.exports = {create}
+const findAll = async (req, res) => {
+    const users = await userService.findAllService()
+
+    if (users.length === 0){
+        res.status(400).send({message: "There are no resgistered users"})
+    }
+
+    res.send(users)
+}
+
+const findById = async (req, res) =>{
+    const id = req.params.id
+
+    
+    if (!mongoose.Types.ObjectId.isValid){
+        return res.status(400).send({message: "Invalid ID"})
+    }
+    
+    const user = await userService.findByIdService(id)
+
+    if(!user){
+        return res.status(400).send({message: "User not found"})
+    }
+
+    res.send(user)
+}
+
+module.exports = {create, findAll, findById}
