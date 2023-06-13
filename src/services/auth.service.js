f234d0bddeb5c54a2be45a3e7cc2c28e6a22b536
MathePLay <Matheus.doer@gmail.com>
2023-06-13 10:53:37 -0300

    End of Class #17

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,9 @@
+import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
 
-const loginService = (email) => User.findOne({email}).select("+password")
+const loginService = (email) => User.findOne({ email }).select("+password")
+
+const generateToken = (id) => jwt.sign({ id }, process.env.SECRET_JWT, { expiresIn: 86400 })
 
 
-export default loginService
+export { loginService, generateToken }
