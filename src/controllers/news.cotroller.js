47a2fd260e1579abb81c6d2f0be972d2a3018961
MathePLay <Matheus.doer@gmail.com>
2023-06-14 09:57:21 -0300

    End of class #18

---
+++ src/controllers/news.cotroller.js
@@ -0,0 +1,41 @@
+import { createService, findAllService } from "../services/news.service.js"
+
+const create = async (req, res) => {
+    try {
+        const { title, text, banner } = req.body
+
+        if (!title || !text || !banner) {
+            res.status(400).send({ message: "submit all fields for registration" })
+        }
+
+        await createService({
+            title,
+            text,
+            banner,
+            user: { _id: "6484e9a25176c8ffe6cbe21d" }
+        })
+
+        res.status(201).send({message: "News Created!"})
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
+}
+
+const findAll = async (req, res) => {
+    try {
+        const news = await findAllService()
+
+        if (news.length === 0) {
+            res.status(400).send({ message: "There are no resgistered news" })
+        }
+
+        res.send(news)
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
+}
+
+export {
+    create,
+    findAll
+}
